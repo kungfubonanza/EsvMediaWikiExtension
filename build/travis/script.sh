d6914a97cab957ff3611f95f8f2d966d1b91c15a
Kung Fu Bonanza <kfb@kungfubonanza.com>
2020-03-04 17:17:08 -0600

    Another shot at getting travis building to work.

--- build/travis/script.sh
+++ build/travis/script.sh
@@ -5,7 +5,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2019 kungfubonanza
+# Copyright (c) 2015-2020 Kungfubonanza
 #        
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
